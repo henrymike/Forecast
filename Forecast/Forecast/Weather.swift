48be2db6133c5526d433e2edd6589a8f63e4512c
Mike Henry <chsmike@gmail.com>
2015-11-03 14:38:56 -0500

    Logging data for a single location

---
+++ Forecast/Forecast/Weather.swift
@@ -0,0 +1,13 @@
+//
+//  Weather.swift
+//  Forecast
+//
+//  Created by Mike Henry on 11/3/15.
+//  Copyright © 2015 Mike Henry. All rights reserved.
+//
+
+import UIKit
+
+class Weather: NSObject {
+
+}
