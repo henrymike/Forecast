282d29ad3286c5b5685f61a98e216c772d6b6420
Mike Henry <chsmike@gmail.com>
2019-12-16 11:28:16 -0500

    Add PanModal pod library

---
+++ Forecast/Forecast/InfoModalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoModalViewController.swift
+//  Forecast
+//
+//  Created by Mike Henry on 12/16/19.
+//  Copyright © 2019 Mike Henry. All rights reserved.
+//
+
+import UIKit
+
+class InfoModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
